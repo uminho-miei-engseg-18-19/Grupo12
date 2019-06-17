b4eabaa39466f44dc3ade6fd27d75cd55fcb00ea
João Alves <joao_pedro.1997@hotmail.com>
2019-06-17 17:48:01 +0100

    Update: extend and validate working for PAdES signature format

--- Projeto/dss-client/dss-client.py
+++ Projeto/dss-client/dss-client.py
@@ -3,10 +3,11 @@ import requests
 import argparse
 import menu
 import json
+import base64
 
-sig_levels = {'CAdES': ['CAdES-BASELINE-T', 'CAdES-BASELINE-LT', 'CAdES-BASELINE-LTA'],
-              'PAdES': ['PAdES-BASELINE-T', 'PAdES-BASELINE-LT', 'PAdES-BASELINE-LTA'], 
-              'XAdES': ['XAdES-BASELINE-T', 'XAdES-BASELINE-LT', 'XAdES-BASELINE-LTA']}
+sig_levels = {'CAdES': ['CAdES_BASELINE_T', 'CAdES_BASELINE_LT', 'CAdES_BASELINE_LTA'],
+              'PAdES': ['PAdES_BASELINE_T', 'PAdES_BASELINE_LT', 'PAdES_BASELINE_LTA'], 
+              'XAdES': ['XAdES_BASELINE_T', 'XAdES_BASELINE_LT', 'XAdES_BASELINE_LTA']}
 
 
 def extendDocumentMenu():
@@ -20,7 +21,10 @@ def extendDocumentMenu():
     input()
     
 def extendDocument(signed_file, original_file, container, sig_format, level):
-    #Temos que passar um certificado para extender a assinatura do documento?
+
+    #Reading file to be signed in byte format
+    with open(signed_file, 'rb') as f:
+        file_bytes = f.read()
 
     if(not level in sig_levels[sig_format]):
         raise Exception('Level {} not valid for signature format {}. Select between {}'.format(level, sig_format, sig_levels[sig_format]))
@@ -28,58 +32,43 @@ def extendDocument(signed_file, original_file, container, sig_format, level):
     #Reading JSON from a File
     with open('application/json/extendDocumentRequest.json', 'r') as json_file:
         params = json.load(json_file)
-        params['asicContainerType'] = str(container)
-        params['signatureLevel'] = str(level)
-    
-    #Request Headers
-    #POST /services/rest/signature/one-document/extendDocument HTTP/1.1
-    #Accept: application/json, application/javascript, text/javascript, text/json
-    #Content-Type: application/json; charset=UTF-8
-    #Host: localhost:8080
-    #Content-Length: 8677
+        params['toExtendDocument']['bytes'] = base64.encodebytes(file_bytes).decode('ascii')
+        if(container == 'No'):
+            params['parameters']['asicContainerType'] = None
+        else:
+            params['parameters']['asicContainerType'] = container
+        params['parameters']['signatureLevel'] = level
+
+    #print(params)
     
     resp = requests.post('http://localhost:8080/services/rest/signature/one-document/extendDocument', json=params)
-    
-    #Response Headers
-    #HTTP/1.1 200 OK
-    #Date: Wed, 21 Nov 2018 09:55:05 GMT
-    #Content-Type: application/json
-    #Transfer-Encoding: chunked
-    #Content-Length: 10552
 
     print(resp.status_code)
 
 def validateSignatureMenu():
-    params = dict()
-    params['service'] = 'validateSignature'
-    main(params)
+    signed_file = input('Signed file: ')
+    original_file = input('Original file: ')
+    validateSignature(signed_file, original_file)
+    print("press enter to continue")
+    input()
 
-def validateSignature():
+def validateSignature(signed_file, original_file):
+
+    #Reading file to be signed in byte format
+    with open(signed_file, 'rb') as f:
+        file_bytes = f.read()
 
     #Reading JSON from a File
     with open('application/json/validateSignatureRequest.json', 'r') as json_file:
         params = json.load(json_file)
-
-    #Request Headers
-    #POST /services/rest/validation/validateSignature HTTP/1.1
-    #Accept: application/json, application/javascript, text/javascript, text/json
-    #Content-Type: application/json; charset=UTF-8
-    #Host: localhost:8080
-    #Content-Length: 7495
+        params['signedDocument']['bytes'] = base64.encodebytes(file_bytes).decode('ascii')
 
     resp = requests.post('http://localhost:8080/services/rest/validation/validateSignature', json=params)
 
-    #Response Headers
-    #HTTP/1.1 200 OK
-    #Date: Wed, 21 Nov 2018 09:55:06 GMT
-    #Content-Type: application/json
-    #Transfer-Encoding: chunked
-    #Content-Length: 31957
-
-    print(resp)
+    print(resp.status_code())
+    #print(json.dumps(resp.json(), indent=4, sort_keys=True))
 
 def main(args):
-    # Falta executar direito quando apenas se executa o programa como: python3 dss-client.py. Se inserirmos 1 para assinar, o programa crasha.
     if args.service is None:
         options = [("Extend a signature",   extendDocumentMenu),
                    ("Validate a signature", validateSignatureMenu),
@@ -89,7 +78,7 @@ def main(args):
     elif args.service == 'extendDocument':
         extendDocument(args.signed_file, args.original_file, args.container, args.sig_format, args.level)
     elif args.service == 'validateSignature':
-        validateSignature()
+        validateSignature(args.signed_file, args.original_file)
         
 
 if __name__ == "__main__":
@@ -99,7 +88,7 @@ if __name__ == "__main__":
     extendDocument_parser = subparsers.add_parser('extendDocument',
                                                     help='the method allows to extend an existing signature to a stronger level')
     extendDocument_parser.add_argument('--signed-file', '-f', required=True, metavar='signed-file', help='signed file')
-    extendDocument_parser.add_argument('--original-file', '-o', required=True, metavar='original-file', help='original file(s)')
+    extendDocument_parser.add_argument('--original-file', '-o', required=False, metavar='original-file', help='original file(s)')
     extendDocument_parser.add_argument('--container', '-c', choices=['No', 'ASiC-S', 'ASiC-E'], default='No', metavar='container', help='container')
     extendDocument_parser.add_argument('--sig-format', choices=['CAdES', 'PAdES', 'XAdES'], required=True, metavar='sig-format', help='signature format')
     extendDocument_parser.add_argument('--level', '-l', required=True, metavar='level', help='level')
